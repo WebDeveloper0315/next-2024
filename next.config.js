37ca84e4c342335843b3bc56ae90377600eceffb
Blackghost <139863785+WebDeveloper0315@users.noreply.github.com>
2024-01-18 07:41:38 +0000

    create/update invoices

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["localhost:3000", "f6m2cv-3000.csb.app"],
+        },
+    },
+}
